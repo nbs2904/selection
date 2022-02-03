fbb5597b64a09e51936a23195724ba318d5945a5
nbs2904 <62153072+nbs2904@users.noreply.github.com>
2022-02-03 23:29:30 +0100

    add: input checks

--- lib/classes/color.ts
+++ lib/classes/color.ts
@@ -1,10 +1,16 @@
+
+const logger = require("@config/log4js").color;
 export class Color {
     public r : number;
     public g : number;
     public b : number;
 
     public constructor(r : number, g : number, b : number) {
-        // TODO check if color is in range otherwise throw error
+        if(r < 0 || g < 0 || b < 0 || r > 255 || g > 255 || b > 255) {
+            logger.error(`Invalid Color parameters: (r: ${r}, g: ${g}, b: ${b})`);
+            throw new Error("Color parameters have to be in the range of 0-255.");                        
+        }
+
         this.r = r;
         this.g = g;
         this.b = b;
