fccf9c687d8a4da0ec196863e6d6bd88311c8e2e
nbs2904 <62153072+nbs2904@users.noreply.github.com>
2022-02-10 18:34:12 +0100

    fix: take weights and biases into account

--- lib/functions/normalise.ts
+++ lib/functions/normalise.ts
@@ -1,8 +1,22 @@
-export function normalise(lowerBound : number, upperBound : number) {
+const CONNECTION_WEIGHT_RANGE = process.env.CONNECTION_WEIGHT_RANGE || 2;
+const BIAS_RANGE = process.env.BIAS_RANGE || 0;
+
+// TODO function might break, since one neuron can receive multiple inputs
+// TODO from different neurons with their respective weight
+// TODO therefore becoming bigger than the boundaries
+
+export function normalise(lowerBound : number, upperBound : number, weightBiasAdapted : boolean) {
     if(upperBound < lowerBound) {
-        throw new Error("uppeBound needs to be bigger than lower bound");
+        throw new Error("upper bound needs to be bigger than lower bound");
     }
 
+    if(weightBiasAdapted) {
+        // ? adapt boundaries according to global CONNECTION_WEIGHT_RANGE and BIAS_RANGE settings
+        const maxAbsoluteBound = Math.max(Math.abs(lowerBound), Math.abs(upperBound));
+        upperBound = (maxAbsoluteBound * (CONNECTION_WEIGHT_RANGE as number) / 2) + (BIAS_RANGE as number / 2);
+        lowerBound = -upperBound;
+    }
+    
     return (input : number) => {
         if(input < lowerBound || upperBound < input) {
             throw new Error("Input value is out of given boundary.");
