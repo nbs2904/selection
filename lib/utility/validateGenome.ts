0245d529ba98863578c2f96a0f4f91ed94318414
nbs2904 <62153072+nbs2904@users.noreply.github.com>
2022-03-27 23:14:10 +0200

    add: validation functions

---
+++ lib/utility/validateGenome.ts
@@ -0,0 +1,38 @@
+import { Genome } from "@interfaces/genome.interface";
+
+const MAX_NUMBER_INNER_NEURONS = +(process.env.MAX_NUMBER_INNER_NEURONS || 4) as number;
+const MIN_NUMBER_GENOME_SIZE =+(process.env.MIN_NUMBER_GENOME_SIZE || 6) as number;
+const MAX_NUMBER_GENOME_SIZE =+(process.env.MAX_NUMBER_GENOME_SIZE || 10) as number;
+const MAX_CONNECTIONS =+(process.env.MAX_CONNECTIONS || 4) as number;
+
+export function validateGenome(genome : Genome) : boolean {
+    // ? check MAX_NUMBER_INNER_NEURONS
+    if(Object.entries(genome.innerNeurons).length > MAX_NUMBER_INNER_NEURONS) {
+        return false;
+    }
+
+    // ? check for genome size
+    const genomeSize = Object.entries(genome.sensors).length + Object.entries(genome.innerNeurons).length + Object.entries(genome.actions).length;
+
+    if(genomeSize < MIN_NUMBER_GENOME_SIZE || genomeSize > MAX_NUMBER_GENOME_SIZE) {
+        return false;
+    }
+
+    // ? MAX_CONNECTIONS
+    // ? check every sensor
+    for (const [, sensor] of Object.entries(genome.sensors)) {
+        if(Object.entries(sensor.connections).length > MAX_CONNECTIONS) {
+            return false;
+        }
+    }
+
+    // ? check every neuron
+    for (const [, neuron] of Object.entries(genome.innerNeurons)) {
+        if(Object.entries(neuron.connections).length > MAX_CONNECTIONS) {
+            return false;
+        }
+    }
+
+
+    return true;
+}
