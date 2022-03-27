92676b459e6f3f7e4e1a14d9fa0df819714b640b
nbs2904 <62153072+nbs2904@users.noreply.github.com>
2022-03-27 23:14:38 +0200

    add: levels

---
+++ lib/levels/corner.level.ts
@@ -0,0 +1,19 @@
+import { Level } from "@customTypes/level.type";
+
+const GRID_SIZE = +(process.env.GRID_SIZE || 50) as number;
+
+export const upperLeft : Level = [
+    [0, 0, GRID_SIZE / 2, GRID_SIZE / 2]
+];
+
+export const upperRight : Level = [
+    [GRID_SIZE / 2, 0, GRID_SIZE, GRID_SIZE / 2]
+];
+
+export const lowerLeft : Level = [
+    [0, GRID_SIZE / 2, GRID_SIZE / 2, GRID_SIZE]
+];
+
+export const lowerRight : Level = [
+    [GRID_SIZE / 2, GRID_SIZE / 2, GRID_SIZE, GRID_SIZE]
+];
---
+++ lib/levels/halfGrid.level.ts
@@ -0,0 +1,19 @@
+import { Level } from "@customTypes/level.type";
+
+const GRID_SIZE = +(process.env.GRID_SIZE || 50) as number;
+
+export const southHalf : Level = [
+    [ 0, GRID_SIZE / 2, GRID_SIZE, GRID_SIZE ]
+];
+
+export const northHalf : Level = [
+    [ 0, 0, GRID_SIZE, GRID_SIZE / 2 ]
+];
+
+export const westHalf : Level = [
+    [ 0, 0, GRID_SIZE / 2, GRID_SIZE ]
+];
+
+export const eastHalf : Level = [
+    [ GRID_SIZE / 2, 0, GRID_SIZE, GRID_SIZE ] 
+];
