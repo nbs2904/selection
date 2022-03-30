3be19cc83a94b9a4463d6ef6dc21451c3bc1d2e8
nbs2904 <62153072+nbs2904@users.noreply.github.com>
2022-03-30 20:40:53 +0200

    add: config interface

---
+++ lib/interfaces/config.interface.ts
@@ -0,0 +1,18 @@
+// TODO keep up to date
+
+export interface Config {
+    PORT: string | number,
+    PXL_HEIGHT: string | number,
+    GRID_SIZE: string | number,
+    FPS: string | number,
+    POPULATION: string | number,
+    STEPS_PER_GENERATION: string | number,
+    MAX_GENERATION: string | number,
+    MUTATE_PROBABILITY: string | number,
+    MAX_NUMBER_INNER_NEURONS: string | number,
+    MIN_NUMBER_GENOME_SIZE: string | number,
+    MAX_NUMBER_GENOME_SIZE: string | number,
+    MAX_CONNECTIONS: string | number,
+    CONNECTION_WEIGHT_RANGE: string | number,
+    BIAS_RANGE: string | number
+}
