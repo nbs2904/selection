f87d90dcd639dd1c6d7e8e0586d4f3c5599e07b0
nbs2904 <62153072+nbs2904@users.noreply.github.com>
2022-03-30 20:42:42 +0200

    fix: coordinate assignment

--- lib/classes/position.ts
+++ lib/classes/position.ts
@@ -1,9 +1,13 @@
+import { randomInteger } from "@utility/randomNumber";
+
+const GRID_SIZE = +(process.env.GRID_SIZE || 50) as number;
 export class Position {
     public x : number;
     public y : number;
 
-    public constructor(x : number, y : number) {
-        this.x = x;
-        this.y = y;
+    public constructor(x? : number, y? : number) {
+
+        this.x = x !== undefined ? x : randomInteger(GRID_SIZE);
+        this.y = y !== undefined ? y : randomInteger(GRID_SIZE);
     }
 }
