0216ff05a2815cc5b3f91d20f953cf38f17862bd
nbs2904 <62153072+nbs2904@users.noreply.github.com>
2022-04-10 20:20:04 +0200

    fix: jsdoc

--- lib/utility/equals.ts
+++ lib/utility/equals.ts
@@ -1,5 +1,5 @@
 /**
- * Comapares two arrays element for element.
+ * Comapares two arrays element for element. Permutations are not considered equal.
  * @param a first array of strings
  * @param b second array of strings
  * @returns boolean - true if the arrays are equal
