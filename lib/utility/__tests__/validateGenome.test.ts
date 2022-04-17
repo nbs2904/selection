830f52d924820e6588889df3652a7fbaec0597b2
nbs2904 <62153072+nbs2904@users.noreply.github.com>
2022-04-17 15:17:07 +0200

    add: unit tests

--- lib/utility/__tests__/validateGenome.test.ts
+++ lib/utility/__tests__/validateGenome.test.ts
@@ -1,9 +1,308 @@
+import { Genome } from "@interfaces/genome.interface";
 import { validateGenome } from "@utility/validateGenome";
 
 
+
 describe("Utility - validateGenome", () => {
-    test.todo("Too many inner neurons");
-    test.todo("Genoem too big");
-    test.todo("Too many connetcions");
-    test.todo("Valid genome");
+    test("Too many inner neurons", () => {
+        const genome : Genome = {
+            sensors: {
+                "Age": {
+                    bias: 0,
+                    connections: {
+                        "Neuron 1": 3.127,
+                        "Neuron 2": 2
+                    },
+                },
+            },
+            innerNeurons: {
+                "Neuron 1": {
+                    bias: 0,
+                    connections: {
+                        "Neuron 2": 3.127,
+                    }
+                },
+                "Neuron 2": {
+                    bias: 0,
+                    connections: {
+                        "Neuron 2": 3.127,
+                    }
+                },
+                "Neuron 3": {
+                    bias: 0,
+                    connections: {
+                        "Neuron 2": 3.127,
+                    }
+                },
+                "Neuron 4": {
+                    bias: 0,
+                    connections: {
+                        "Neuron 2": 3.127,
+                    }
+                },
+                "Neuron 5": {
+                    bias: 0,
+                    connections: {
+                        "Neuron 2": 3.127,
+                    }
+                },
+            },
+            actions: {
+                "MoveX": {
+                    bias: 0
+                }
+            }
+        };
+
+        expect(validateGenome(genome)).toBe(false);
+    });
+
+    test("Genome too big", () => {        
+        const genome : Genome = {
+            sensors: {
+                "Age": {
+                    bias: 0,
+                    connections: {
+                        "Neuron 1": 3.127,
+                        "Neuron 2": 2
+                    },
+                },
+                "XPos": {
+                    bias: 0,
+                    connections: {
+                        "Neuron 1": 3.127,
+                        "Neuron 2": 2
+                    },
+                }
+            },
+            innerNeurons: {
+                "Neuron 1": {
+                    bias: 0,
+                    connections: {
+                        "Neuron 2": 3.127,
+                    }
+                },
+                "Neuron 2": {
+                    bias: 0,
+                    connections: {
+                        "Neuron 2": 3.127,
+                    }
+                },
+                "Neuron 3": {
+                    bias: 0,
+                    connections: {
+                        "Neuron 2": 3.127,
+                    }
+                },
+                "Neuron 4": {
+                    bias: 0,
+                    connections: {
+                        "Neuron 2": 3.127,
+                    }
+                }
+            },
+            actions: {
+                "MoveX": {
+                    bias: 0
+                },
+                "MoveY": {
+                    bias: 0
+                },
+                "MoveBwd": {
+                    bias: 0
+                },
+                "MoveFwd": {
+                    bias: 0
+                },
+                "MoveRnd": {
+                    bias: 0
+                }
+            }
+        };
+
+        expect(validateGenome(genome)).toBe(false);
+    });
+
+    test("Genome too small", () => {       
+        const genome : Genome = {
+            sensors: {
+                "Age": {
+                    bias: 0,
+                    connections: {
+                        "Neuron 1": 3.127,
+                        "Neuron 2": 2
+                    },
+                }
+            },
+            innerNeurons: {
+                "Neuron 1": {
+                    bias: 0,
+                    connections: {
+                        "Neuron 2": 3.127,
+                    }
+                }
+            },
+            actions: {
+                "MoveX": {
+                    bias: 0
+                }
+            }
+        };
+
+        expect(validateGenome(genome)).toBe(false);
+    });
+
+    test("Sensor has too many connections", () => {
+        const genome : Genome = {
+            sensors: {
+                "Age": {
+                    bias: 0,
+                    connections: {
+                        "Neuron 1": 3.127,
+                        "Neuron 2": 2,
+                        "Neuron 3": 2,
+                        "Neuron 4": 2,
+                        "MoveX": 2,
+                    },
+                }
+            },
+            innerNeurons: {
+                "Neuron 1": {
+                    bias: 0,
+                    connections: {
+                        "Neuron 2": 3.127,
+                    }
+                },
+                "Neuron 2": {
+                    bias: 0,
+                    connections: {
+                        "Neuron 2": 3.127,
+                    }
+                },
+                "Neuron 3": {
+                    bias: 0,
+                    connections: {
+                        "Neuron 2": 3.127,
+                    }
+                },
+                "Neuron 4": {
+                    bias: 0,
+                    connections: {
+                        "Neuron 2": 3.127,
+                    }
+                }
+            },
+            actions: {
+                "MoveX": {
+                    bias: 0
+                }
+            }
+        };
+
+        expect(validateGenome(genome)).toBe(false);
+    });
+
+    test("Inner neuron has too many connetcions", () => {     
+        const genome : Genome = {
+            sensors: {
+                "Age": {
+                    bias: 0,
+                    connections: {
+                        "Neuron 1": 3.127
+                    },
+                }
+            },
+            innerNeurons: {
+                "Neuron 1": {
+                    bias: 0,
+                    connections: {
+                        "Neuron 2": 3.127,
+                        "Neuron 3": 2,
+                        "Neuron 4": 2,
+                        "MoveX": 2,
+                        "MoveY": 2
+                    }
+                },
+                "Neuron 2": {
+                    bias: 0,
+                    connections: {
+                        "Neuron 2": 3.127,
+                    }
+                },
+                "Neuron 3": {
+                    bias: 0,
+                    connections: {
+                        "Neuron 2": 3.127,
+                    }
+                },
+                "Neuron 4": {
+                    bias: 0,
+                    connections: {
+                        "Neuron 2": 3.127,
+                    }
+                }
+            },
+            actions: {
+                "MoveX": {
+                    bias: 0
+                },
+                "MoveY": {
+                    bias: 0
+                }
+            }
+        };
+
+        expect(validateGenome(genome)).toBe(false);
+    });
+
+    test("Valid genome", () => {
+        const genome : Genome = {
+            sensors: {
+                "Age": {
+                    bias: 0,
+                    connections: {
+                        "Neuron 1": 3.127
+                    },
+                }
+            },
+            innerNeurons: {
+                "Neuron 1": {
+                    bias: 0,
+                    connections: {
+                        "Neuron 2": 3.127,
+                        "Neuron 3": 2,
+                        "Neuron 4": 2
+                    }
+                },
+                "Neuron 2": {
+                    bias: 0,
+                    connections: {
+                        "Neuron 2": 3.127,
+                    }
+                },
+                "Neuron 3": {
+                    bias: 0,
+                    connections: {
+                        "Neuron 2": 3.127,
+                    }
+                },
+                "Neuron 4": {
+                    bias: 0,
+                    connections: {
+                        "Neuron 2": 3.127,
+                    }
+                }
+            },
+            actions: {
+                "MoveX": {
+                    bias: 0
+                },
+                "MoveY": {
+                    bias: 0
+                }
+            }
+        };
+
+        expect(validateGenome(genome)).toBe(true);
+    });
 });
