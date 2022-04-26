ab571fd975f2b2687e000d83a177f0ed7d2fe0e4
nbs2904 <62153072+nbs2904@users.noreply.github.com>
2022-04-26 19:02:41 +0200

    add: simulation mock

---
+++ lib/classes/__mocks__/simulation.mock.ts
@@ -0,0 +1,7 @@
+// * classes
+import { Node } from "@classes/node";
+import { Position } from "@classes/position";
+
+export const updateNodePositionMock = function (node : Node, newPosition : Position) : boolean {
+    return true;
+};
