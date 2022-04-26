c7aee4ecce14c43c25dbf14e6ffd5b404b80b260
nbs2904 <62153072+nbs2904@users.noreply.github.com>
2022-04-26 19:03:12 +0200

    add: node class unit tests

--- lib/classes/__tests__/node.test.ts
+++ lib/classes/__tests__/node.test.ts
@@ -1,17 +1,20 @@
-import { northHalf } from "@levels/halfGrid.level";
-import { Simulation } from "../simulation";
-import { Color } from "../color";
-import { Node } from "../node";
-import { Position } from "../position";
+// * classes
+import { Color } from "@classes/color";
+import { Node } from "@classes/node";
+import { Position } from "@classes/position";
+
+// * interfaces
 import { Genome } from "@interfaces/genome.interface";
 
-let simulation : Simulation;
+// * mocks
+import { updateNodePositionMock } from "@classes/__mocks__/simulation.mock";
+
 let node : Node;
 let genome : Genome;
+let spy : jest.SpyInstance;
 
 beforeEach(() => {
     // TODO Mock simulation class
-    simulation = new Simulation(northHalf);
     genome = {
         sensors: {
             "XPos": {
@@ -87,15 +90,20 @@ beforeEach(() => {
         }
     };
     node = new Node("123", genome, new Position(10, 1), new Color(100, 150, 200));
+    node.updateNodePosition = updateNodePositionMock;
 
-    simulation.spawnNode(node);
+    // ? add test function to manipulate lastDirection property of node.senation object
+    node["updateLastDirection"] = function (x : number, y : number) {
+        this.sensation.lastDirection = new Position(x, y);
+    };
+
+    spy = jest.spyOn(node, "updateNodePosition").mockReturnValue(true);
 });
 
 describe("Classes - Node: Instantiation", () => {
     test("Initialize Node Instance", () => {
-        node = new Node("456", genome, new Position(0, 0), new Color(200, 200, 200));
-        
-        expect(node.id).toBe("456");
+        expect(node).toBeInstanceOf(Node);
+        expect(node.id).toBe("123");
         expect(node.getSensation.age).toBe(0);
 
         expect(typeof node.id).toBe("string");
@@ -119,9 +127,111 @@ describe("Classes - Node: Getter Functions", () => {
         expect(node.getColor.g).toBe(150);
         expect(node.getColor.b).toBe(200);
     });
+
+    test("Get Node Age", () => {
+        expect(node.getAge).toBe(0);
+        expect(typeof node.getAge).toBe("number");
+    });
+
+    test("Get Node Sensation", () => {
+        expect(node.getSensation.age).toBe(0);
+        expect(node.getSensation.x).toBe(10);
+        expect(node.getSensation.y).toBe(1);
+    });
 });
 
 describe("Classes - Node: Action Functions", () => {
-    test.todo("Move Node in x-direction");
-    test.todo("Move Node in y-direction");
+    test("moveX", () => {
+        node.moveX(1);
+        expect(node.x).toBe(11);
+        expect(node.y).toBe(1);
+        expect(spy).toHaveBeenCalledTimes(1);
+
+        node.moveX(-1);
+        expect(node.x).toBe(10);
+        expect(node.y).toBe(1);
+        expect(spy).toHaveBeenCalledTimes(2);
+
+        expect(() => node.moveX(0)).rejects.toThrow();
+        expect(() => node.moveX(2)).rejects.toThrow();
+        expect(() => node.moveX(-2)).rejects.toThrow();
+
+        expect(node.x).toBe(10);
+        expect(node.y).toBe(1);
+    });
+
+    test("moveY", () => {
+        node.moveY(1);
+        expect(node.x).toBe(10);
+        expect(node.y).toBe(2);
+        expect(spy).toHaveBeenCalledTimes(1);
+
+        node.moveY(-1);
+        expect(node.x).toBe(10);
+        expect(node.y).toBe(1);
+        expect(spy).toHaveBeenCalledTimes(2);
+
+        expect(() => node.moveY(0)).rejects.toThrow();
+        expect(() => node.moveY(2)).rejects.toThrow();
+        expect(() => node.moveY(-2)).rejects.toThrow();
+        
+        expect(node.x).toBe(10);
+        expect(node.y).toBe(1);
+    });
+
+    test("moveFwd", () => {
+        // ? set lastDirection to (1, 0)
+        node["updateLastDirection"](1, 0);
+        node.moveFwd();
+        expect(node.x).toBe(11);
+        expect(node.y).toBe(1);
+        expect(spy).toHaveBeenCalledTimes(1);
+
+        // ? set lastDirection to (0, 1)
+        node["updateLastDirection"](0, 1);
+        node.moveFwd();
+        expect(node.x).toBe(11);
+        expect(node.y).toBe(2);
+        expect(spy).toHaveBeenCalledTimes(2);
+
+        // ? set lastDirection to (1, 1)
+        node["updateLastDirection"](1, 1);
+        expect(() => node.moveFwd()).rejects.toThrow();
+        expect(node.x).toBe(11);
+        expect(node.y).toBe(2);
+        expect(spy).toHaveBeenCalledTimes(2);
+    });
+
+    test("moveBwd", () => {
+        // ? set lastDirection to (1, 0)
+        node["updateLastDirection"](1, 0);
+        node.moveBwd();
+        expect(node.x).toBe(9);
+        expect(node.y).toBe(1);
+        expect(spy).toHaveBeenCalledTimes(1);
+
+        // ? set lastDirection to (0, 1)
+        node["updateLastDirection"](0, 1);
+        node.moveBwd();
+        expect(node.x).toBe(9);
+        expect(node.y).toBe(0);
+        expect(spy).toHaveBeenCalledTimes(2);
+
+        // ? set lastDirection to (1, 1)
+        node["updateLastDirection"](1, 1);
+        expect(() => node.moveBwd()).rejects.toThrow();
+        expect(node.x).toBe(9);
+        expect(node.y).toBe(0);
+        expect(spy).toHaveBeenCalledTimes(2);
+    });
+    
+    test.todo("moveRnd");
 });
+
+describe("Classes - Node: Other functions", () => {
+    test.todo("act");
+    test.todo("copyGenome");
+    test.todo("reproduce");
+    test.todo("storeGenome");
+});
+
