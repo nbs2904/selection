c4d00d0791e9fc76137d517b91b318b1ce4f80a9
nbs2904 <62153072+nbs2904@users.noreply.github.com>
2022-05-04 18:58:09 +0200

    add: todo

--- gulpfile.js
+++ gulpfile.js
@@ -25,6 +25,7 @@ gulp.task("compile", () => {
 
 gulp.task("resolve-alias", run("npx tscpaths -p tsconfig.json -s . -o ./build"));
 
+// TODO update config files
 gulp.task("copy-config", () => {
     return gulp.src([ "index.html", ".env", "nodemon.json", "./config*/**/*", "./public*/**/*" ])
         .pipe(gulp.dest("build"));
