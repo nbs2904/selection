b7c06e8465b15013570ceac35d72644bea2a918e
nbs2904 <62153072+nbs2904@users.noreply.github.com>
2022-05-04 18:57:08 +0200

    add: jest setup file & move into config folder

---
+++ config/jest/jest.config.js
@@ -0,0 +1,12 @@
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+
+const { pathsToModuleNameMapper } = require("ts-jest");
+const { compilerOptions } = require("./../../tsconfig.json");
+
+module.exports = {
+    rootDir: "./../../",
+    preset: "ts-jest",
+    testEnvironment: "node",
+    moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, { prefix: "<rootDir>" }),
+    setupFiles: ["./config/jest/jest.setup.js"],
+};
