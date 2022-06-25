350ec848be5cf574a6440e34700f2296e2b99fc4
nbs2904 <62153072+nbs2904@users.noreply.github.com>
2022-06-25 01:31:14 +0100

    update: graph plotting

--- lib/python/config/config.py
+++ lib/python/config/config.py
@@ -6,7 +6,8 @@ SENSOR_COLOR = "lightblue"
 NEURON_COLOR = "grey"
 ACTION_COLOR = "red"
 
-SENSOR_NAMES = ["XPos", "YPos", "Age"]
-ACTION_NAMES = ["MoveX", "MoveY", "MoveFwd", "MoveBwd", "MoveRnd"]
+# TODO import json name files
+SENSOR_NAMES = ["XPos", "YPos", "Age", "Random"]
+ACTION_NAMES = ["MoveX", "MoveY", "MoveFwd", "MoveBwd", "MoveRnd", "MoveL", "MoveR", "MoveN", "MoveS", "MoveE", "MoveW", "Kill"]
 
 PLOT_OUTPUT_EXTENSION = ".png"
--- lib/python/main.py
+++ lib/python/main.py
@@ -6,7 +6,7 @@ from lib.get_graph import get_graph
 from lib.plot_graph import plot_graph
 
 
-path = "./lib/simulations/20220325161239/"
+path = "./lib/simulations/20220625002326/"
 output_base = "./lib/genomes/images/"
 files = [file for file in os.listdir(path) if file.endswith(".json")]
 
