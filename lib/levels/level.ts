8d645319f69033cb057ce50fdec6e5730c0ffc61
nbs2904 <62153072+nbs2904@users.noreply.github.com>
2022-06-26 16:01:26 +0100

    add: single level export

---
+++ lib/levels/level.ts
@@ -0,0 +1,10 @@
+// * levels
+import * as cornerLevels from "@levels/corner.level";
+import * as halfGridLevels from "@levels/halfGrid.level";
+import * as middleLevels from "@levels/middle.level";
+
+export default {
+    ...cornerLevels,
+    ...halfGridLevels,
+    ...middleLevels
+};
