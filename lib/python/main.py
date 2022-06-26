bbf76b43fe24527452830d1b62168797e0b27f1a
nbs2904 <62153072+nbs2904@users.noreply.github.com>
2022-06-26 19:47:53 +0100

    add: comment

--- lib/python/main.py
+++ lib/python/main.py
@@ -5,8 +5,11 @@ from config.config import PLOT_OUTPUT_EXTENSION
 from lib.get_graph import get_graph
 from lib.plot_graph import plot_graph
 
+# ? This file can be used to generate a graph image from the json files, which are stored after every successful simulation.
+# ? Each json file contains one genome of a surviving node.
 
-path = "./lib/simulations/20220625002326/"
+# ? Just change the path to the direcotry of the simulation results.
+path = "./lib/simulations/20220626183507/"
 output_base = "./lib/genomes/images/"
 files = [file for file in os.listdir(path) if file.endswith(".json")]
 
