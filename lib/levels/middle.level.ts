5a23b08c50bddf226910c9d4958157f79a20af55
nbs2904 <62153072+nbs2904@users.noreply.github.com>
2022-06-26 16:02:14 +0100

    fix: level coordinates

--- lib/levels/middle.level.ts
+++ lib/levels/middle.level.ts
@@ -4,9 +4,9 @@ import { Level } from "@customTypes/level.type";
 const GRID_SIZE = +(process.env.GRID_SIZE || 50) as number;
 
 export const hoizontalMiddle : Level = [
-    [ GRID_SIZE / 2, 0, GRID_SIZE / 2, GRID_SIZE ]
+    [ GRID_SIZE / 4, 0, 3 * GRID_SIZE / 4, GRID_SIZE ]
 ];
 
 export const verticalMiddle : Level = [
-    [ 0, GRID_SIZE / 2, GRID_SIZE, GRID_SIZE / 2 ]
+    [ 0, GRID_SIZE / 4, GRID_SIZE, 3 * GRID_SIZE / 4 ]
 ];
